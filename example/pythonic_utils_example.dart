f33fe5fa7d44e2211325830eca89b9074b314283
volodymyrb <volodymyrb@fornova.net>
2021-09-29 21:19:35 +0300

    init

---
+++ example/pythonic_utils_example.dart
@@ -0,0 +1,6 @@
+import 'package:pythonic_utils/pythonic_utils.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pythonic_utils.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pythonic_utils;
+
+export 'src/pythonic_utils_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
